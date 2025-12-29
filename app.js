937803448f6451d526eaa1119f7dd6eb3d1155ae
SPOTWEBDEV <spotwebdev.com@gmail.com>
2025-12-29 09:50:18 +0100

    setup

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+// const authRoutes = require("./routes/auth.routes");
+// const userRoutes = require("./routes/user.routes");
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+// app.use("/api/auth", authRoutes);
+// app.use("/api/user", userRoutes);
+
+// Health check
+app.get("/", (req, res) => {
+  res.json({ status: "API running 🚀" });
+});
+
+module.exports = app;
